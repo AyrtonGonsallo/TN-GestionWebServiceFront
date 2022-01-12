deb577558fe6eff0f3886bf89a6d8a5616414226
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2022-01-12 19:27:14 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientDetailsComponent } from './clients/client-details/client-details.component';
+import { CreateClientsComponent } from './clients/create-clients/create-clients.component';
 import { GetClientsInfosComponent } from './clients/get-clients-infos/get-clients-infos.component';
 
 const routes: Routes = [
   {path:"getClients",component:GetClientsInfosComponent},
   {path:"getClient/:id",component:ClientDetailsComponent},
+  {path:"addClient",component:CreateClientsComponent},
   {path:"",redirectTo:"getClients",pathMatch:"full"}
 ];
 
